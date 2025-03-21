b546e1e9b485ee0562d2fc7985a972b45a4bf81a
Ritesh Ghimire <reetesh@reetesh-2.local>
2025-03-21 11:02:43 -0500

    added favorite and option to send email

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 
 
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/api/v1";
+//export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/api/v1";
+export const API_BASE_URL = "http://52.90.131.91/waa-backend/api/v1";
--- src/pages/Customer.jsx
+++ src/pages/Customer.jsx
@@ -1,4 +1,6 @@
 import Property from '../components/Property'
+
+import Properties from '../components/Properties'
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { API_BASE_URL } from '../config';
@@ -8,48 +10,13 @@ import { apiRequest } from '../request';
 export default function Customer() {
     const navigate = useNavigate();
     const [currentTab, setCurrentTab] = useState('properties');
-    const [availableProperties, setAvailableProperties] = useState([]);
-    const [filteredProperties, setFilteredProperties] = useState([]);
 
     const [offerHistory, setOfferHistory] = useState([]);
     const [savedProperties, setSavedProperties] = useState([]);
     const [selectedProperty, setSelectedProperty] = useState(null);
-    const [offerPrice, setOfferPrice] = useState('');
-    const [offerMessage, setOfferMessage] = useState('');
-
-    const [searchName, setSearchName] = useState('');
-    const [searchLocation, setSearchLocation] = useState('');
-    const [minPrice, setMinPrice] = useState('');
-    const [maxPrice, setMaxPrice] = useState('');
-    const [sortOption, setSortOption] = useState('');
 
 
-    useEffect(() => {
-        filterProperties();
-    }, [searchName, searchLocation, minPrice, maxPrice, availableProperties, sortOption]);
-
-
-    const filterProperties = () => {
-        let filtered = availableProperties.filter((property) => {
-            return (
-                (searchName === '' || property.name.toLowerCase().includes(searchName.toLowerCase())) &&
-                (searchLocation === '' || property.address.toLowerCase().includes(searchLocation.toLowerCase())) &&
-                (minPrice === '' || parseFloat(property.price) >= parseFloat(minPrice)) &&
-                (maxPrice === '' || parseFloat(property.price) <= parseFloat(maxPrice))
-            );
-        });
-
-        if (sortOption === 'price-asc') {
-            filtered.sort((a, b) => a.price - b.price);
-        } else if (sortOption === 'price-desc') {
-            filtered.sort((a, b) => b.price - a.price);
-        } else if (sortOption === 'date-asc') {
-            filtered.sort((a, b) => new Date(a.created) - new Date(b.created));
-        } else if (sortOption === 'date-desc') {
-            filtered.sort((a, b) => new Date(b.created) - new Date(a.created));
-        }
-        setFilteredProperties(filtered);
-    };
+    const [propertyKey, setPropertyKey] = useState(0);
 
 
     const userDetails = JSON.parse(localStorage.getItem('user'));
@@ -64,18 +31,11 @@ export default function Customer() {
         return words[0][0].toUpperCase() + words[1][0].toUpperCase();
     };
 
-    const handleChange = (e) => {
-        if (e.target.id == 'offerprice') {
-            console.log('offerprice');
-            setOfferPrice(e.target.value);
-        } else if (e.target.id == 'offermessage') {
-            console.log('message');
-            setOfferMessage(e.target.value);
-        }
-    }
+
+
 
     const cancelOffer = async (id) => {
-        const data = await apiRequest(`${API_BASE_URL}/offers/${id}`, 'PATCH');
+        await apiRequest(`${API_BASE_URL}/offers/${id}`, 'PATCH', { 'status': 'CANCELLED' });
         const updatedOfferHistory = offerHistory.filter(offer => offer.id !== id);
         setOfferHistory(updatedOfferHistory);
 
@@ -93,27 +53,12 @@ export default function Customer() {
         setCurrentTab('saved');
     }
     const fetchAvailableProperties = async () => {
-        const data = await apiRequest(`${API_BASE_URL}/properties`, 'GET');
-        setAvailableProperties(data);
-        setFilteredProperties(data);
-
+        setPropertyKey(prevKey => prevKey + 1);
         setCurrentTab('properties');
     }
-    useEffect(() => {
-        fetchAvailableProperties();
-    }, []);
 
-    const addOffer = async () => {
-        const postData = {
-            'offerAmount': offerPrice,
-            'message': offerMessage,
-            'propertyId': selectedProperty.id
-        };
-        const data = await apiRequest(`${API_BASE_URL}/offers/create`, 'POST', postData);
-        setOfferMessage('');
-        setOfferPrice('');
+    
 
-    }
     const handleLogout = () => {
         localStorage.removeItem("user");
         localStorage.removeItem("access_token");
@@ -158,58 +103,10 @@ export default function Customer() {
                     </div>
 
                     {currentTab === 'properties' &&
-                        <div className="w-full md:w-3/4">
+                        <div className="w-full">
 
                             <h2 className="text-blue-400 text-center text-3xl border-b border-solid border-gray-300 mb-7 pb-3">Available Properties</h2>
-                            <div className="mb-4 grid grid-cols-1 md:grid-cols-5 gap-4">
-                                <input
-                                    type="text"
-                                    placeholder="Search by Name"
-                                    value={searchName}
-                                    onChange={(e) => setSearchName(e.target.value)}
-                                    className="p-2 border border-gray-300 rounded-md"
-                                />
-                                <input
-                                    type="text"
-                                    placeholder="Search by Location"
-                                    value={searchLocation}
-                                    onChange={(e) => setSearchLocation(e.target.value)}
-                                    className="p-2 border border-gray-300 rounded-md"
-                                />
-                                <input
-                                    type="number"
-                                    placeholder="Min Price"
-                                    value={minPrice}
-                                    onChange={(e) => setMinPrice(e.target.value)}
-                                    className="p-2 border border-gray-300 rounded-md"
-                                />
-                                <input
-                                    type="number"
-                                    placeholder="Max Price"
-                                    value={maxPrice}
-                                    onChange={(e) => setMaxPrice(e.target.value)}
-                                    className="p-2 border border-gray-300 rounded-md"
-                                />
-                                <select
-                                    value={sortOption}
-                                    onChange={(e) => setSortOption(e.target.value)}
-                                    className="p-2 border border-gray-300 rounded-md"
-                                >
-                                    <option value="">Sort By</option>
-                                    <option value="price-asc">Price: Low to High</option>
-                                    <option value="price-desc">Price: High to Low</option>
-                                    <option value="date-asc">Date: Oldest First</option>
-                                    <option value="date-desc">Date: Newest First</option>
-                                </select>
-                            </div>
-
-                            <div className="border-solid border-t border-gray-300 pt-4 mt-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-10">
-                                {filteredProperties &&
-                                    filteredProperties.map((property) => (
-                                        <Property property={property} setSelectedProperty={setSelectedProperty} key={`property-${property.id}`} />
-                                    )
-                                    )}
-                            </div>
+                            <Properties key={propertyKey} />
                         </div>
                     }
 
@@ -235,7 +132,9 @@ export default function Customer() {
                                                     <h3 className="text-lg font-medium text-gray-900 truncate">
                                                         {offer.property?.name}
                                                     </h3>
-                                                    <div className="mt-3">{offer.status}</div>
+                                                    <div className="mt-3">
+                                                        <span className={offer.status}>{offer.status}</span>
+                                                    </div>
                                                     <div className="my-4 font-bold">Offfer Price : ${offer.offerAmount}</div>
 
                                                     <div className="my-4 font-bold">Original Price : ${offer.property?.price}</div>
@@ -265,9 +164,9 @@ export default function Customer() {
                             <div className="w-full">
                                 <h2 className="text-blue-400 text-center text-3xl border-b border-solid border-gray-300 mb-7 pb-3">Saved Properties</h2>
 
-                                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-10">
+                                <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-10">
                                     {savedProperties.map((property) => (
-                                        <Property property={property} setSelectedProperty={setSelectedProperty} key={`saved-${property.id}`} />
+                                        <Property property={property} setSelectedProperty={setSavedProperties} key={`saved-${property.id}`} />
                                     ))
                                     }
                                 </div>
@@ -277,58 +176,7 @@ export default function Customer() {
 
                 </div >
 
-                {selectedProperty &&
-                    <div
-                        className="fixed inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center z-50 overflow-hidden"
-                    >
-                        <div className="bg-white p-6 rounded-lg w-11/12 md:w-3/4 lg:w-2/3 max-w-screen-lg max-h-[90vh]">
-                            <div className="flex justify-between items-center mb-6">
-                                <h2 className="text-2xl text-center">Send Offer</h2>
-                                <button onClick={() => setSelectedProperty(null)}
-                                    className="text-gray-500 hover:text-gray-700 text-2xl font-bold"
-                                >
-                                    ×
-                                </button>
-                            </div>
-                            <div className="flex gap-4">
-                                <div className="flex-1">
-                                    <div>
-                                        <label className="block mb-2 text-sm font-medium text-gray-700">
-                                            Price
-                                        </label>
-                                        <input
-                                            type="text"
-                                            id="offerPrice"
-                                            value={offerPrice}
-                                            onChange={(e) => setOfferPrice(e.target.value)}
-                                            name="price"
-                                            placeholder="Enter Your Offer Price"
-                                            className="w-full p-2.5 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
-                                        />
-                                    </div>
-                                    <div className="mt-5">
-                                        <label className="block mb-2 text-sm font-medium text-gray-700">
-                                            Message
-                                        </label>
-                                        <textarea onChange={(e) => setOfferMessage(e.target.value)} value={offerMessage} id="offerMessage" className="w-full p-2.5 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"></textarea>
-                                    </div>
-                                    <button onClick={addOffer} className="bg-blue-800 text-white p-4">Send Offer</button>
-                                </div>
-                                <div className="flex-1">
-                                    <img
-                                        src={selectedProperty.fileResources?.length > 0
-                                            ? `${API_BASE_URL}/file-resources/${selectedProperty.fileResources[0].storageKey}`
-                                            : "/lazy.png"}
-                                        alt={selectedProperty.name}
-                                        className="property-image w-full h-48 object-cover" />
-                                    <h2>{selectedProperty.name}</h2>
-                                    <strong>Price : ${selectedProperty.price}</strong>
-                                </div>
 
-                            </div>
-                        </div>
-                    </div>
-                }
 
             </>
         </>
